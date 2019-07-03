b7179e39e09a01ef581b9f6f83d129e67047d904
Shabnam Ghasemirad <sghasemi@wks-40-86.mpi-sws.org>
2019-07-03 15:37:46 +0200

    initialize

---
+++ SF_series/lf/.coqdeps.d
@@ -0,0 +1,76 @@
+Auto.vo Auto.glob Auto.v.beautified: Auto.v Maps.vo Imp.vo
+Auto.vio: Auto.v Maps.vio Imp.vio
+AutoTest.vo AutoTest.glob AutoTest.v.beautified: AutoTest.v Auto.vo
+AutoTest.vio: AutoTest.v Auto.vio
+Basics.vo Basics.glob Basics.v.beautified: Basics.v
+Basics.vio: Basics.v
+BasicsTest.vo BasicsTest.glob BasicsTest.v.beautified: BasicsTest.v Basics.vo
+BasicsTest.vio: BasicsTest.v Basics.vio
+Bib.vo Bib.glob Bib.v.beautified: Bib.v
+Bib.vio: Bib.v
+BibTest.vo BibTest.glob BibTest.v.beautified: BibTest.v Bib.vo
+BibTest.vio: BibTest.v Bib.vio
+Extraction.vo Extraction.glob Extraction.v.beautified: Extraction.v ImpCEvalFun.vo Imp.vo ImpParser.vo Maps.vo
+Extraction.vio: Extraction.v ImpCEvalFun.vio Imp.vio ImpParser.vio Maps.vio
+ExtractionTest.vo ExtractionTest.glob ExtractionTest.v.beautified: ExtractionTest.v Extraction.vo
+ExtractionTest.vio: ExtractionTest.v Extraction.vio
+Imp.vo Imp.glob Imp.v.beautified: Imp.v Maps.vo
+Imp.vio: Imp.v Maps.vio
+ImpCEvalFun.vo ImpCEvalFun.glob ImpCEvalFun.v.beautified: ImpCEvalFun.v Imp.vo Maps.vo
+ImpCEvalFun.vio: ImpCEvalFun.v Imp.vio Maps.vio
+ImpCEvalFunTest.vo ImpCEvalFunTest.glob ImpCEvalFunTest.v.beautified: ImpCEvalFunTest.v ImpCEvalFun.vo
+ImpCEvalFunTest.vio: ImpCEvalFunTest.v ImpCEvalFun.vio
+ImpParser.vo ImpParser.glob ImpParser.v.beautified: ImpParser.v Maps.vo Imp.vo
+ImpParser.vio: ImpParser.v Maps.vio Imp.vio
+ImpParserTest.vo ImpParserTest.glob ImpParserTest.v.beautified: ImpParserTest.v ImpParser.vo
+ImpParserTest.vio: ImpParserTest.v ImpParser.vio
+ImpTest.vo ImpTest.glob ImpTest.v.beautified: ImpTest.v Imp.vo
+ImpTest.vio: ImpTest.v Imp.vio
+IndPrinciples.vo IndPrinciples.glob IndPrinciples.v.beautified: IndPrinciples.v ProofObjects.vo
+IndPrinciples.vio: IndPrinciples.v ProofObjects.vio
+IndPrinciplesTest.vo IndPrinciplesTest.glob IndPrinciplesTest.v.beautified: IndPrinciplesTest.v IndPrinciples.vo
+IndPrinciplesTest.vio: IndPrinciplesTest.v IndPrinciples.vio
+IndProp.vo IndProp.glob IndProp.v.beautified: IndProp.v Logic.vo
+IndProp.vio: IndProp.v Logic.vio
+IndPropTest.vo IndPropTest.glob IndPropTest.v.beautified: IndPropTest.v IndProp.vo
+IndPropTest.vio: IndPropTest.v IndProp.vio
+Induction.vo Induction.glob Induction.v.beautified: Induction.v Basics.vo
+Induction.vio: Induction.v Basics.vio
+InductionTest.vo InductionTest.glob InductionTest.v.beautified: InductionTest.v Induction.vo
+InductionTest.vio: InductionTest.v Induction.vio
+Lists.vo Lists.glob Lists.v.beautified: Lists.v Induction.vo
+Lists.vio: Lists.v Induction.vio
+ListsTest.vo ListsTest.glob ListsTest.v.beautified: ListsTest.v Lists.vo
+ListsTest.vio: ListsTest.v Lists.vio
+Logic.vo Logic.glob Logic.v.beautified: Logic.v Tactics.vo
+Logic.vio: Logic.v Tactics.vio
+LogicTest.vo LogicTest.glob LogicTest.v.beautified: LogicTest.v Logic.vo
+LogicTest.vio: LogicTest.v Logic.vio
+Maps.vo Maps.glob Maps.v.beautified: Maps.v
+Maps.vio: Maps.v
+MapsTest.vo MapsTest.glob MapsTest.v.beautified: MapsTest.v Maps.vo
+MapsTest.vio: MapsTest.v Maps.vio
+Poly.vo Poly.glob Poly.v.beautified: Poly.v Lists.vo
+Poly.vio: Poly.v Lists.vio
+PolyTest.vo PolyTest.glob PolyTest.v.beautified: PolyTest.v Poly.vo
+PolyTest.vio: PolyTest.v Poly.vio
+Postscript.vo Postscript.glob Postscript.v.beautified: Postscript.v
+Postscript.vio: Postscript.v
+PostscriptTest.vo PostscriptTest.glob PostscriptTest.v.beautified: PostscriptTest.v Postscript.vo
+PostscriptTest.vio: PostscriptTest.v Postscript.vio
+Preface.vo Preface.glob Preface.v.beautified: Preface.v
+Preface.vio: Preface.v
+PrefaceTest.vo PrefaceTest.glob PrefaceTest.v.beautified: PrefaceTest.v Preface.vo
+PrefaceTest.vio: PrefaceTest.v Preface.vio
+ProofObjects.vo ProofObjects.glob ProofObjects.v.beautified: ProofObjects.v IndProp.vo
+ProofObjects.vio: ProofObjects.v IndProp.vio
+ProofObjectsTest.vo ProofObjectsTest.glob ProofObjectsTest.v.beautified: ProofObjectsTest.v ProofObjects.vo
+ProofObjectsTest.vio: ProofObjectsTest.v ProofObjects.vio
+Rel.vo Rel.glob Rel.v.beautified: Rel.v IndProp.vo
+Rel.vio: Rel.v IndProp.vio
+RelTest.vo RelTest.glob RelTest.v.beautified: RelTest.v Rel.vo
+RelTest.vio: RelTest.v Rel.vio
+Tactics.vo Tactics.glob Tactics.v.beautified: Tactics.v Poly.vo
+Tactics.vio: Tactics.v Poly.vio
+TacticsTest.vo TacticsTest.glob TacticsTest.v.beautified: TacticsTest.v Tactics.vo
+TacticsTest.vio: TacticsTest.v Tactics.vio
